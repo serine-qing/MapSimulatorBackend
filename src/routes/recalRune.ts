213ba37c8308e9362edade959d0be8f7378f34b9
lqzhu <463109762@qq.com>
2025-08-29 16:56:50 +0800

    全息作战矩阵 变量字段、变量颜色解析

--- src/routes/recalRune.ts
+++ src/routes/recalRune.ts
@@ -13,9 +13,20 @@ const parseLevelData = (data: {[key: string]: any}) => {
   }
 }
 
+const blue = "#0080ff";
+const red = "#ff2020";
+
 const parseRune = (rune: any) => {
   const {runeId, sortId, essential, exclusiveGroupId, runeIcon, packedRune} = rune;
   const {points, mutexGroupKey, description, runes} = packedRune;
+  const descriptionArr = 
+    description
+      .replaceAll(`<@recalrune.pos>`, `<span style="color:${blue}">`)
+      .replaceAll(`<@recalrune.nag>`, `<span style="color:${red}">`)
+      .replaceAll(`</>`, `</span>`)
+      .split("\r\n");
+
+  parseDescription(descriptionArr, runes);
   return {
     runeId,
     sortId,
@@ -23,9 +34,39 @@ const parseRune = (rune: any) => {
     exclusiveGroupId,
     runeIcon,
     points,
-    description: description.replace(/<@[\s\S]*?>|<\/[\s\S]*?>|\\n/g, "").split("\r\n"),
+    description: descriptionArr,
     runes
   }
+}
+
+const parseDescription = (descArr: any[], runes: any[]) => {
+  descArr.forEach((text, index) => {
+    const regex = /{([^:}+]+)(?::\d+%?)?}/g;
+    let match;
+    
+    while(match = regex.exec(text)){
+      const hasPercent = match[0].includes("%");   //是否有百分号
+      const hasColon = match[0].includes(":0");   //是否有:0
+
+      let key = match[1];
+            
+      let value;
+
+      runes.find((rune: any) => {
+        value = rune.blackboard.find((item: any) => item.key === key)?.value;
+        return value;
+      })
+
+
+
+      if(value){
+        descArr[index] = hasPercent? 
+          descArr[index].replace(`{${key}:0%}`, value * 100 + "%")
+          : hasColon ? descArr[index].replace(`{${key}:0}`, value) 
+            : descArr[index].replace(`{${key}}`, value);
+      }
+    }
+  })
 }
 
 Object.values(recalRuneData).forEach((season: any) => {
